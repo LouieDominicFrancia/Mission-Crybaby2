03cf770fd16eb807c937a694bbb34af8f3cf09de
adriangelyn <93594824+adriangelyn@users.noreply.github.com>
2021-11-25 15:24:58 +0800

    27-28

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -56,11 +56,11 @@ public class PlayerController : MonoBehaviour
     {
         if(other.gameObject.tag == "Enemy")
         {
-            Frog frog = other.gameObject.GetComponent<Frog>();
+            Enemy enemy = other.gameObject.GetComponent<Enemy>();
 
             if(state == State.falling)
             {
-                frog.JumpedOn();
+                enemy.JumpedOn();
                 Jump();
             }
             else
