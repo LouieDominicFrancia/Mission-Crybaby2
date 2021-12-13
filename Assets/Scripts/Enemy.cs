59778b38862f765e4b2f1b877961026cf38ce30c
Bloosillus <theunknownbruh25@gmail.com>
2021-12-14 03:19:42 +0900

    Enemy entity death animation update
    when enemy dies the explosion moves where the frog supposed to fall off from jumping = fixed

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,9 +6,11 @@ public class Enemy : MonoBehaviour
 {
     protected Animator anim;
     protected AudioSource death;
+    protected Rigidbody2D rb;
 
     protected virtual void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         death = GetComponent<AudioSource>();
     }
@@ -17,6 +19,9 @@ public class Enemy : MonoBehaviour
     {
         death.Play();
         anim.SetTrigger("Death");
+        rb.velocity = Vector2.zero;
+        rb.bodyType = RigidbodyType2D.Kinematic;
+        GetComponent<Collider2D>().enabled = false;
     }
 
     private void Death()
